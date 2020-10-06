2b4d1dfb24817b69febe0f4130299776527521f2
Tabunov Egor <Vonubat@gmail.com>
2020-10-06 23:30:14 +0300

    feat: reverse-int task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  // приводим к абсолютному значению
+  let absValues = Math.abs(n);
+  // преобразуем в строку
+  let str = '' + absValues;
+  //  разделяем строку на массив
+  let tempArr1 = str.split('');
+  // делаем реверс массива
+  let tempArr2 = tempArr1.reverse();
+  // создаём из массива строку
+  let reversedStr = tempArr2.join('');
+  return reversedStr;
 }
